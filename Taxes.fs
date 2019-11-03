22d8c2e57f8fa483dfd8b4bd3ccdb0ab4f1139e4
sebastian <sebastian.ferngren@gmail.com>
2019-11-03 21:27:11 +0100

    add taxes module

---
+++ Taxes.fs
@@ -0,0 +1,11 @@
+﻿module Taxes
+    let amountToTax pay limit roof = pay - limit |> min (roof - limit)
+    let rateAmount (amount:double) (rate:double) = amount * rate
+    let amountAfterTax pay limit roof rate = amountToTax pay limit roof |> rateAmount rate |> max 0.
+
+    let minTax pay = amountAfterTax pay 10000. 30000. 0.1
+    let midTax pay = amountAfterTax pay 30000. 100000. 0.25
+    let highTax pay = amountAfterTax pay 100000. pay  0.4
+    let totalTax pay = [minTax;midTax;highTax] |> List.map (fun x -> x pay)  |> List.sum
+    let taxedAmount salary = salary |> totalTax
+
