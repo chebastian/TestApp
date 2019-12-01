4943f98de24b1fc351cee1a4a40967a9ec5c5f05
sebastian <sebastian.ferngren@gmail.com>
2019-12-01 23:35:04 +0100

    add advent 2019

---
+++ Advent2019/Program.fs
@@ -0,0 +1,36 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.IO;
+
+let lines = File.ReadLines("./input/one.txt")
+
+
+let fuelCount input:float = 
+    let x = (input / 3.0) |> floor 
+    x - 2.0
+
+let rec fuelReq input:float =
+    let newFuele = fuelCount input
+    if newFuele > 0.0 then
+        newFuele + (fuelReq newFuele)
+    else
+        0.0
+
+let dayOne input = 
+    let xx = input |> Seq.map (fun a -> float a |> fuelCount)
+    xx |> Seq.sum
+
+let dayTwo input = 
+    let xx = input |> Seq.map (fun a -> float a |> fuelReq)
+    xx |> Seq.sum
+
+    // another comment
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    let frst = dayOne lines
+    let snd = dayTwo lines
+
+    0 // return an integer exit code
