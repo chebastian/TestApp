f0abd331b7cafb0f0c829ad172df47f33929f14d
sebastian <sebastian.ferngren@gmail.com>
2019-12-03 23:28:16 +0100

    matches opcodes

--- Advent2019/Program.fs
+++ Advent2019/Program.fs
@@ -27,6 +27,22 @@ let dayTwo input =
 
     // another comment
 
+module DayTwo  = 
+    let delim = ','
+    type OPCode = 
+    | Add = 1
+    | Mul = 2
+    | Error = -1
+    | Halt = 99
+
+    let GetOpCode (input:string) = 
+        let split = input.Split(delim) 
+        match split |> Array.toList with
+        | x::xs when x = "1" -> OPCode.Add
+        | x::xs when x = "2" -> OPCode.Mul
+        | x::xs when x = "99" -> OPCode.Halt
+        | _ -> OPCode.Error
+
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
